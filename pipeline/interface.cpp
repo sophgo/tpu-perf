abb5771df24edd24addf6d56a1773af47d79dc59
taochen.lu <taochen.lu@sophgo.com>
2022-10-28 20:46:03 +0800

    fix

--- pipeline/interface.cpp
+++ pipeline/interface.cpp
@@ -277,7 +277,6 @@ blob_info_t *get_output_info(unsigned runner_id, unsigned *num)
         blob.name = net_info->output_names[i];
         bm_shape_t &s = net_info->stages[0].output_shapes[i];
         blob.num_dims = s.num_dims;
-        blob.dtypes = (net_info->output_dtypes)[i];
         memcpy(blob.dims, s.dims, s.num_dims * sizeof(int));
         blob.scale = (net_info->output_scales)[i];
     }
